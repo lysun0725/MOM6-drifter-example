c8e013069039a4ad34b6bd5ee700f74cab9d9998
Matthew Harrison <Matthew.Harrison@noaa.gov>
2017-08-08 17:32:25 -0400

    fix initialize_drifters.py

--- ocean_only/double_gyre/initialize_drifters.py
+++ ocean_only/double_gyre/initialize_drifters.py
@@ -2,13 +2,13 @@
 
 import netCDF4 as nc
 f=nc.Dataset('drifters.res.nc','w')
-f.createDimension('i')
-iv=f.createVariable('i','i4',('i'))
+f.createDimension('i',None)
+ndv=f.createVariable('num_drifters','i',())
 lonv=f.createVariable('lon','f8',('i'))
 latv=f.createVariable('lat','f8',('i'))
 depthv=f.createVariable('depth','f8',('i'))
 timev=f.createVariable('time','f8',('i'))
-idv=f.createVariable('id','i4',('i'))
+idv=f.createVariable('id','f8',('i'))
 
 lonv.long_name='longitude'
 lonv.units='degrees_E'
@@ -19,7 +19,7 @@ depthv.units='meters'
 timev.units='days since 1900-01-01 00:00:00'
 
 
-iv[:]=[1,2,3,4]
+ndv.assignValue(4)
 lonv[:]=[5.,10.,15.,20.]
 latv[:]=[40.,40.,40.,40.]
 depthv[:]=[15.,15.,15.,15.]
